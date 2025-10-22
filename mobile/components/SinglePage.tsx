73d79e61b0a93bc0e9f2b4c1388b600938f3adff
STEFAN SUDY <stefan.sudy@student.tugraz.at>
2025-10-22 22:15:11 +0200

    dito

---
+++ mobile/components/SinglePage.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { View, Text, TextInput, Button, FlatList, Pressable, StyleSheet, Dimensions } from "react-native";
+import { ToDoEntry } from "../../shared/types/ToDoEntry";
+import { styles } from "../styles/styles";
+import { TodoItem } from "./TodoItem";
+import { AddView } from "./AddView";
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+export default function SinglePage({data, API_URL}: {data: ToDoEntry[], API_URL?: string}) {
+  const [todos, setTodos] = useState<ToDoEntry[]>(data);
+  const [addViewVisible, setAddViewVisible] = useState(false);
+
+  const handleAdd = (text: string) => {
+    setTodos([...todos, { id: todos.length+1, text, done: false , creationDate: new Date()}]);
+    setAddViewVisible(false);
+  }
+
+  const checkTodo = (id: number) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo));
+  }
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  return (
+    <View style={styles.root}>
+        <AddView 
+          isVisible={addViewVisible} 
+          onAdd={handleAdd} 
+          onClose={() => setAddViewVisible(false)}/>
+          <FlatList
+            data={todos}
+            keyExtractor={item => item.id.toString()}
+            contentContainerStyle={styles.todoList}
+            renderItem={({ item }) => 
+              <TodoItem item={item} onDelete={deleteTodo} onPress={checkTodo}/>
+            }/>
+      <Pressable onPress={() => setAddViewVisible(true)} style={styles.roundPressableButton}>
+        <Ionicons name="add" size={Dimensions.get('window').height * 0.05} color="white" />
+      </Pressable>
+    </View>
+  );
+}
---
+++ mobile/components/TodoItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet, Dimensions } from "react-native";
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import { styles } from "../styles/styles";
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+export const TodoItem = ({ item, onPress, onDelete }: any) => {
+  const renderRightActions = () => (
+    <View style={styles.deleteContainer}>
+      <Text style={styles.deleteText}>REMOVE</Text>
+    </View>
+  );
+
+  return (
+    <Swipeable renderRightActions={renderRightActions} onSwipeableOpen={() => onDelete(item.id)} containerStyle={{ width: '100%' }}>
+      <View style={styles.item}>
+        <Text style={styles.todoText} numberOfLines={1}>{item.text}</Text>
+        <Pressable onPress={() => onPress(item.id)}>
+          <Ionicons name={item.done ? "checkmark-circle" : "ellipse-outline"} size={Dimensions.get('window').height * 0.06} color={item.done ? "green" : "gray"} />
+        </Pressable>
+      </View>
+    </Swipeable>
+  );
+}
