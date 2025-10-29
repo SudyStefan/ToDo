370bb02087e8b0851f8126490920fdb4f0e3155f
Sudy Stefan <stefan.sudy@student.tugraz.at>
2025-10-29 16:41:11 +0100

    commit from laptop after lab

--- mobile/components/PeriodicItem.tsx
+++ mobile/components/PeriodicItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Pressable, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, Pressable, StyleSheet, Dimensions, Animated } from "react-native";
 import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 import { styles, colors } from "../styles/styles";
 import Ionicons from '@expo/vector-icons/Ionicons'
@@ -9,11 +9,17 @@ type PeriodicItemProp = {
   item: ToDoEntry,
   onPress: Function,
   onSwipe: Function,
-  swipeLeft?: boolean
-  swipeRight?: boolean
 }
 
-export const PeriodicItem = ({ item, onPress, onSwipe, swipeLeft = false, swipeRight = false}: PeriodicItemProp) => {
+export const PeriodicItem = ({ item, onPress, onSwipe }: PeriodicItemProp) => {
+  const [prog, setProg] = useState<number>(0);
+
+  useEffect(() => {
+    setProg(() => 
+      (new Date().getSeconds() - item.lastChecked!.getSeconds()) / item.period! * 100 <= 100 
+      ? (new Date().getSeconds() - item.lastChecked!.getSeconds()) / item.period! * 100 : 100 );
+  }, []);
+
   const renderActions = (text: string) => (
     <View style={styles.deleteContainer}>
       <Text style={styles.deleteText}>{text}</Text>
@@ -22,21 +28,16 @@ export const PeriodicItem = ({ item, onPress, onSwipe, swipeLeft = false, swipeR
 
   return (
     <Swipeable 
-    renderLeftActions={swipeLeft ? () => renderActions("CHECK") : undefined}
-    dragOffsetFromLeftEdge={swipeLeft ? undefined : Number.MAX_VALUE}
-    renderRightActions={swipeRight ? () => renderActions("DELETE") : undefined}
-    dragOffsetFromRightEdge={swipeRight ? undefined : Number.MAX_VALUE}
+    renderRightActions={() => renderActions("DELETE")}
+    dragOffsetFromRightEdge={Number.MAX_VALUE}
     onSwipeableOpen={() => onSwipe(item.id)} 
     containerStyle={{ width: '100%' }} 
     testID="TodoItem">
       <View style={styles.item}>
+        <View 
+        testID="ProgBar" 
+        style={{position: 'relative', width: 500, backgroundColor: colors.soxred }} />
         <Text style={styles.itemText} numberOfLines={1}>{item.text}</Text>
-        <Pressable onPress={() => onPress(item.id)}>
-          <Ionicons 
-          name={item.status === Status.Done ? "checkmark-circle" : "ellipse-outline"} 
-          size={Dimensions.get('window').height * 0.06} 
-          color={item.status === Status.Done ? colors.seattlegreen : colors.soxred} />
-        </Pressable>
       </View>
     </Swipeable>
   );
